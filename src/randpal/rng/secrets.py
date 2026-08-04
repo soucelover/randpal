59063a54091a4523770d3fb4713ecf913f5aefe9
Soucelover <mr.ktotov@gmail.com>
2026-08-04 11:55:45 +0300

    Use os.urandom to implement SecretsRandom provider

---
+++ src/randpal/rng/secrets.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import secrets
+
+from typing_extensions import override
+
+from . import RandomProvider
+
+
+class SecretsRandom(RandomProvider):
+    _generator: secrets.SystemRandom
+
+    def __init__(self, generator: secrets.SystemRandom | None = None) -> None:
+        if generator is None:
+            generator = secrets.SystemRandom()
+
+        self._generator = generator
+
+    @override
+    def randbytes(self, n: int) -> bytes:
+        return self._generator.randbytes(n)
+
+    @override
+    def randbits(self, k: int) -> int:
+        return self._generator.getrandbits(k)
