7f288dbab8d47c6b45765232e07437694948a2e9
Soucelover <mr.ktotov@gmail.com>
2026-09-04 15:23:38 +0300

    Implement chance command

--- src/randpal/cli/commands.py
+++ src/randpal/cli/commands.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import sys
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -38,6 +39,34 @@ def command_float(
             print(number)  # noqa: T201
         case "verbose":
             print(f"And so, the random number is... {number}!", end="\n\n")  # noqa: T201
+        case _:
+            raise NotImplementedError
+
+
+def command_chance(
+    args: GeneratorCommandArgs, factory: RandomGeneratorFactory
+) -> None:
+    generator: FloatGenerator = factory.create_rng(args.provider)
+
+    if not 0 <= args.probability <= 100:  # noqa: PLR2004
+        print(  # noqa: T201
+            "ERROR: The probability must lie between 0 and 100.",
+            file=sys.stderr,
+        )
+        return
+
+    result = generator.chance(args.probability / 100)
+
+    match args.output_format:
+        case "concise":
+            print(("no", "yes")[result])  # noqa: T201
+        case "verbose":
+            if result:
+                print('The odds has said "yes"!')  # noqa: T201
+            else:
+                print("A chance word didn't turn in your favour...")  # noqa: T201
+        case "exit-code":
+            raise SystemExit(int(not result))
         case _:
             raise NotImplementedError
 
